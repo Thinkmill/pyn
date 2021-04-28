d10444f5ee8c358ac061140d5a2782a4d2926687
mitchellhamilton <mitchell@hamil.town>
2021-04-28 21:30:44 +1000

    Use thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 use serde::Deserialize;
-use std::env;
-use std::fmt;
-use std::fs;
-use std::io;
-use std::path::Path;
-use std::process::Command;
-use std::{collections::HashMap, ffi::OsStr};
+use std::{
+    collections::HashMap,
+    env,
+    ffi::OsStr,
+    fmt, fs, io,
+    path::{Path, PathBuf},
+    process::Command,
+};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Could not find a package root")]
+    CouldNotFindPackageRoot,
+    #[error("No lockfile could be found. If you haven't used a package manager in this project yet, please do that first to generate a lockfile")]
+    CouldNotFindLockfile,
+    #[error("Could not find a script or binary named {0}")]
+    CouldNotFindScriptOrBinary(String),
+    #[error("Child process exited with {0}")]
+    ChildProcessExit(i32),
+    #[error("{0}")]
+    Io(#[from] io::Error),
+    #[error("{0}")]
+    Serde(#[from] serde_json::Error),
+}
+
+type Result<T> = std::result::Result<T, Error>;
 
 enum PackageManager {
     PNPM,
@@ -52,26 +72,24 @@ impl Default for PackageJson {
     }
 }
 
-fn get_package_root(path: &Path) -> io::Result<PackageJson> {
+fn get_package_root(path: &Path) -> Result<PackageJson> {
     let package_json_path = path.join(Path::new("package.json"));
     if package_json_path.exists() {
         let contents = fs::read_to_string(package_json_path)?;
         let package_json: PackageJson = serde_json::from_str(&contents)?;
-        return Ok(package_json);
+        Ok(package_json)
     } else if let Some(parent_path) = path.parent() {
-        return get_package_root(parent_path);
+        get_package_root(parent_path)
     } else {
-        println!("Could not find a package root");
-        std::process::exit(1)
+        Err(Error::CouldNotFindPackageRoot)
     }
 }
 
-fn get_project_root(path: &Path) -> io::Result<ProjectRoot> {
+fn get_project_root(path: &Path) -> Result<ProjectRoot> {
     let dir = fs::read_dir(path)?;
 
     for entry in dir {
         let entry = entry?;
-
         let package_manager = match entry.file_name().to_str() {
             Some("yarn.lock") => PackageManager::Yarn,
             Some("pnpm-lock.yaml") => PackageManager::PNPM,
@@ -85,14 +103,13 @@ fn get_project_root(path: &Path) -> io::Result<ProjectRoot> {
         return Ok(project);
     }
     if let Some(parent_path) = path.parent() {
-        return get_project_root(parent_path);
+        get_project_root(parent_path)
+    } else {
+        Err(Error::CouldNotFindLockfile)
     }
-
-    eprintln!("No lockfile could be found. If you haven't used a package manager in this project yet, please do that first to generate a lockfile");
-    std::process::exit(1)
 }
 
-fn run_package_manager<S: AsRef<OsStr>>(path: &Path, args: &[S]) -> io::Result<()> {
+fn run_package_manager<S: AsRef<OsStr>>(path: &Path, args: &[S]) -> Result<()> {
     let project = get_project_root(path)?;
     eprintln!(
         "🧞 Found {} project at {}",
@@ -110,57 +127,62 @@ fn run_package_manager<S: AsRef<OsStr>>(path: &Path, args: &[S]) -> io::Result<(
     Ok(())
 }
 
-fn find_binary_location(current_dir: &Path, binary: &str) -> std::path::PathBuf {
+fn find_binary_location(current_dir: &Path, binary: &str) -> Result<PathBuf> {
     let mut path_string = String::from("node_modules/.bin/");
     path_string.push_str(binary);
     let path = current_dir.join(Path::new(&path_string));
     if path.exists() {
-        return path;
+        Ok(path)
     } else if let Some(parent) = current_dir.parent() {
-        return find_binary_location(parent, binary);
+        find_binary_location(parent, binary)
     } else {
-        eprintln!("Could not find a script or binary named {}", binary);
-        std::process::exit(1)
+        Err(Error::CouldNotFindScriptOrBinary(binary.to_owned()))
     }
 }
 
 // TODO: do script running in rust rather than offloading to the package manager
 // (i'm not totally sure about that)
-fn run_script_or_binary(current_dir: &Path, args: &[String]) -> io::Result<()> {
+fn run_script_or_binary(current_dir: &Path, args: &[String]) -> Result<()> {
     let pkg = get_package_root(current_dir)?;
     let bin = args[0].as_ref();
-    if pkg.scripts.contains_key(bin) {
+    let mut child = if pkg.scripts.contains_key(bin) {
         let project = get_project_root(current_dir)?;
-        let mut child = project
+        project
             .package_manager
             .cmd()
             .arg("run")
             .args(args)
-            .spawn()?;
-        let status = child.wait()?;
-        if let Some(code) = status.code() {
-            std::process::exit(code)
-        }
+            .spawn()?
     } else {
-        let binary = find_binary_location(current_dir, &bin);
-        let mut child = Command::new(binary).args(&args[1..]).spawn()?;
-        let status = child.wait()?;
-        if let Some(code) = status.code() {
-            std::process::exit(code)
-        }
+        let binary = find_binary_location(current_dir, &bin)?;
+        Command::new(binary).args(&args[1..]).spawn()?
+    };
+    let status = child.wait()?;
+    let code = status.code().unwrap_or(1);
+    if code == 0 {
+        Ok(())
+    } else {
+        Err(Error::ChildProcessExit(code))
     }
-
-    Ok(())
 }
 
-fn main() -> io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    let current_dir = env::current_dir()?;
-    match args.get(0) {
+    let current_dir = env::current_dir().unwrap();
+    let result = match args.get(0) {
         Some(first_arg) => match first_arg.as_str() {
             "add" | "install" | "remove" | "why" => run_package_manager(&current_dir, &args),
             _ => run_script_or_binary(&current_dir, &args),
         },
         _ => run_package_manager(&current_dir, &["install"]),
+    };
+    if let Err(err) = result {
+        match err {
+            Error::ChildProcessExit(code) => std::process::exit(code),
+            _ => {
+                eprintln!("{}", err);
+                std::process::exit(1);
+            }
+        }
     }
 }
